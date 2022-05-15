0005cb2be7b0ef375eb92563e75c8d066d760da2
meatlhy-kamiya <a_kamiya@mealthy.me>
2022-05-15 19:33:06 +0900

    開発実績の修正と追加

--- lib/home/contents_view.dart
+++ lib/home/contents_view.dart
@@ -34,15 +34,26 @@ String markdownDataOtherDevelopment = '''
   - graphQLによる通信
 
 - FCM実装
-  - FirebaseMessaging（Android、iOS)
+  - Firebase Messaging（Android、iOS)
     - Androidのネイティブコードにて受信後に音楽ファイルを自動再生させる機能を実装
-  - FirebaseCrashlytics（Android、iOS)
+  - Firebase Crashlytics（Android、iOS)
   - Firebase Remote Config(Android、iOS)
   
 - GoogleMap実装
-  - ライブラリを導入し、GPS情報を収集して奇跡の描画も実装
+  - ライブラリを導入し、GPS情報を収集して軌跡の描画も実装
+  - ピンの描画
 
 - Push通知
   - APNsのPush通知実装
   - iOSのPushにて、Payload指定の着信音を鳴らす実装
+  
+- 内部データ保存について
+  - SQLite実装
+  - SharedPrefereneces実装
+  
+- ネイティブコードからFlutterへの換装経験
+- Flutter1.xからFlutter2.xへの換装経験
+- QRコード読取り機能実装
+- ライブラリを用いたカレンダーカスタマイズ実装
+- ライブアリを用いた音声の録音再生機能実装
 ''';
